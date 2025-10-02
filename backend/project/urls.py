c356846da1d742f88dfc5f77b46f4cc2e838537d
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-10-02 00:28:02 -0300

    Add initial Django app structure with basic files and URL routing

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('app.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
