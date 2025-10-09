832a5565d819e655ee09a4023637c770ea7e68de
Gabriel Pagani de Souza <gabriel.souza@gruposmi.com.br>
2025-10-09 15:16:46 -0300

    Refactor CPF validation logic

--- backend/app/validators.py
+++ backend/app/validators.py
@@ -10,11 +10,19 @@ def valid_cpf(value):
     if len(value) != 11 or value == value[0] * 11:
         raise ValidationError('Invalid CPF.')
 
-    sum1 = sum(int(value[i]) * (10 - i) for i in range(9))
-    digit1 = (sum1 * 10 % 11) % 10
+    sum1 = digit1 = 0
+    for i in range(9):
+        sum1 += int(value[i]) * (10 - i)
+    digit1 = 11 - (sum1 % 11)
+    if digit1 >= 10:
+        digit1 = 0
 
-    sum2 = sum(int(value[i]) * (11 - i) for i in range(10))
-    digit2 = (sum2 * 10 % 11) % 10
+    sum2 = digit2 = 0
+    for i in range(10):
+        sum2 += int(value[i]) * (11 - i)
+    digit2 = 11 - (sum2 % 11)
+    if digit2 >= 10:
+        digit2 = 0
 
     if int(value[9]) != digit1 or int(value[10]) != digit2:
         raise ValidationError('Invalid CPF.')
