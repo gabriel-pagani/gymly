e4fdb17b4d1044f618638b8b944ec1672530d9d5
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-11-05 22:12:07 -0300

    Add utility function to generate Metabase dashboard URLs with JWT authentication

---
+++ backend/app/utils/metabase.py
@@ -0,0 +1,30 @@
+import jwt
+import time
+from project.local_settings import METABASE_SECRET_KEY, METABASE_SITE_URL
+
+
+token_cache = {}
+
+
+def generate_dashboard_url(dashboard_id):
+    current_time = time.time()
+    cached = token_cache.get(dashboard_id)
+    if cached and current_time < cached['exp']:
+        return cached['url']
+
+    payload = {
+        "resource": {"dashboard": dashboard_id},
+        "params": {},
+        "exp": round(time.time()) + (60 * 60)
+    }
+
+    token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+
+    url = f"{METABASE_SITE_URL}/embed/dashboard/{token}#bordered=true&titled=true"
+
+    token_cache[dashboard_id] = {
+        "url": url,
+        "exp": round(time.time()) + (60 * 60)
+    }
+
+    return url
