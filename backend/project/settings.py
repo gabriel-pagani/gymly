1e45ae59d7026f936d2943c87123a765cc6035f9
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-11-05 22:26:11 -0300

    Update CSRF_TRUSTED_ORIGINS to include the correct URL

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1',
     'http://localhost',
-    'https://gymly.gabriel-pagani.space',
+    'https://dashboards-center.gabriel-pagani.space/',
 ]
 
 INSTALLED_APPS = [
