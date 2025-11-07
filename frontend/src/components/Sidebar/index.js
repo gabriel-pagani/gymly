07e410774c0f3c6a99ddc5d6a324915f3cd6e772
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-11-06 21:02:18 -0300

    Update Sidebar component to render a div and integrate it into Home component

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 
 function Sidebar() {
-  return <></>;
+  return <div></div>;
 }
 
 export default Sidebar;
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./styles.css";
+import Sidebar from "../../components/Sidebar";
 
 function Home() {
-  return <></>;
+  return (
+    <>
+      <Sidebar />
+      <iframe title="Dashboard" src="" />
+    </>
+  );
 }
 
 export default Home;
