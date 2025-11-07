0b4619ac795d7f5d3336730034f9baf5c3068eed
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-11-06 22:55:59 -0300

    Update Sidebar component structure and styles: enhance layout with top, menu, and bottom sections; set font family in index.css

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import "../../styles/sidebar.css";
 
 function Sidebar() {
-  return <div id="sidebar"></div>;
+  return (
+    <div id="sidebar">
+      <div id="top"></div>
+      <nav id="menu"></nav>
+      <div id="bottom"></div>
+    </div>
+  );
 }
 
 export default Sidebar;
