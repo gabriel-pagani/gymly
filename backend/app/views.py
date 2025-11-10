7701006b6fc4e5b2f8b0937fcd892f936adc5852
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-11-10 15:26:11 -0300

    Add sectors action to DashboardsViewSet for grouped dashboard retrieval

--- backend/app/views.py
+++ backend/app/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
+from collections import OrderedDict
 
 
 class UsersViewSet(viewsets.ModelViewSet):
@@ -33,6 +34,24 @@ class DashboardsViewSet(viewsets.ModelViewSet):
             Q(users=user) | Q(groups__in=user.groups.all())
         ).order_by('id').distinct()
 
+    @action(detail=False, methods=['get'])
+    def sectors(self, request):
+        queryset = self.get_queryset().order_by('sector', 'title')
+
+        sectors = OrderedDict()
+        for dashboard_data in queryset:
+            sector = dashboard_data.sector
+            if sector not in sectors:
+                sectors[sector] = []
+
+            dashboard = {
+                'id': dashboard_data.id,
+                'title': dashboard_data.title,
+            }
+            sectors[sector].append(dashboard)
+
+        return Response(sectors)
+
     @action(detail=True, methods=['get'], url_path='favorite')
     def favorite(self, request, pk=None):
         user = request.user
@@ -45,7 +64,7 @@ class DashboardsViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset()
         if not queryset.filter(pk=pk).exists():
             return Response({'detail': 'You do not have permission to perform this action.'}, status=status.HTTP_403_FORBIDDEN)
-        
+
         if dashboard.fav_by.filter(id=user.id).exists():
             dashboard.fav_by.remove(user)
             return Response({'favorited': False}, status=status.HTTP_200_OK)
