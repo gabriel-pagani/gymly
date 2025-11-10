200f976bc51d4cb7d726c631a7cbc00d12fc7f8c
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-11-10 14:18:06 -0300

    Refactor Sidebar component: restructure JSX for improved readability and organization

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -1,15 +1,61 @@
 import React from "react";
 import "../../styles/sidebar.css";
 
+const sectorsData = [
+  { id: "sector-1", dashboards: ["dash-1-1", "dash-1-2"] },
+  { id: "sector-2", dashboards: ["dash-2-1", "dash-2-2"] },
+  { id: "sector-3", dashboards: ["dash-3-1", "dash-3-2"] },
+];
+
+const menuItemsData = ["item-1", "item-2"];
+
 function Sidebar() {
   return (
-    <div id="sidebar">
-      <div id="top"></div>
+    <aside id="sidebar">
+      <header id="top">
+        <div id="logo">
+          <a href="/">
+            <img alt="Logo Grupo SMI" src="/path/to/logo.png" />
+          </a>
+        </div>
+      </header>
+
       <div className="divider" />
-      <nav id="menu"></nav>
+
+      <nav id="menu">
+        <div id="search-bar"></div>
+        <ul id="sectors-list">
+          {sectorsData.map((sector) => (
+            <li key={sector.id} className="sector">
+              {sector.id}
+              <ul className="dashboards-list">
+                {sector.dashboards.map((dashboard) => (
+                  <li key={dashboard} className="dashboard">
+                    {dashboard}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
       <div className="divider" />
-      <div id="bottom"></div>
-    </div>
+
+      <footer id="bottom">
+        <div id="user-menu">
+          user.name
+          <ul id="user-menu-list">
+            {menuItemsData.map((item) => (
+              <li key={item} className="user-menu-item">
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div id="toggle-button">=</div>
+      </footer>
+    </aside>
   );
 }
 
