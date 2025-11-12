71382555f5b3c6ff89aba2ea12d55df94db9c80c
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-11-12 12:19:26 -0300

    Add 'me' action to UsersViewSet for retrieving current user data

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
 from .serializers import UsersSerializer, DashboardsSerializer
 from .models import Users, Dashboards
 from django.db.models import Q
@@ -20,6 +20,11 @@ class UsersViewSet(viewsets.ModelViewSet):
         if user.is_superuser or user.has_perm('app.view_users'):
             return Users.objects.all().order_by('id')
         return Users.objects.none()
+
+    @action(detail=False, methods=['get'], url_path='me', permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
 
 
 class DashboardsViewSet(viewsets.ModelViewSet):
